85fa1e36047edfed700867cc59ceaaeb1e48c394
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-13 09:51:07 +0300

    feat: Refactor data preparation for improved flexibility
    This commit refactors the `DataPreparation` class to enhance
    flexibility and clarity. It introduces an `__init__` method for
    configuring the class with a custom data path and a list of NLTK
    resources to download. The `download_nltk_resources` method is
    updated to accept a mandatory list of resources, removing the
    default resource set and promoting explicit configuration. This change
    enhances the class's adaptability to diverse project needs and
    improves code maintainability.

--- train_ml_model/data_preparation.py
+++ train_ml_model/data_preparation.py
@@ -9,32 +9,36 @@ logger = logging.getLogger(__name__)
 
 
 class DataPreparation:
+    def __init__(
+        self,
+        path_to_data_for_training: str = "train.csv",
+        nltk_resources: Optional[List[str]] = None,
+    ) -> None:
+        self.path_to_data_for_training = path_to_data_for_training
+        self.nltk_resources = (
+            nltk_resources
+            if nltk_resources is not None
+            else ["punkt", "stopwords", "wordnet"]
+        )
+
     @staticmethod
-    def download_nltk_resources(resources: Optional[List[str]] = None):
+    def download_nltk_resources(resources: List[str]):
         """
-        Downloads specified NLTK resources or a default set if none are
-        provided.
+        Downloads specified NLTK resources.
 
         Parameters:
         resources (Optional[List[str]]): A list of NLTK resource names to
-                                         download. If None, downloads
-                                         a default set of resources including
-                                         "punkt", "stopwords", and "wordnet".
+                                         download.
 
         This method logs the download process and ensures that the required
         NLTK resources are available for text processing tasks.
         """
-        if not resources:
-            logger.info(
-                "Resources not specified. Downloading base resources..."
-            )
-            resources = ["punkt", "stopwords", "wordnet"]
         for resource in resources:
             logger.info(f"Downloading resource: {resource}")
             nltk.download(resource)
 
     @staticmethod
-    def download_data_for_training(path: str = "train.csv") -> DataFrame:
+    def download_data_for_training(path: str) -> DataFrame:
         """
         Downloads a CSV file containing data for training the model, and
         returns a Pandas DataFrame with the downloaded data.
