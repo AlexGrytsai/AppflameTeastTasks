b2fcd8ba21eea05ea22d5173f5b4a385cdc8f748
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-13 15:29:28 +0300

    feat: Expose ModelML in train_ml_model package
    This change makes the `ModelML` class accessible from the
    `train_ml_model` package by adding it to the `__all__` list
    in the `__init__.py` file. This allows other modules to import
    and use the `ModelML` class directly from the package.

--- train_ml_model/__init__.py
+++ train_ml_model/__init__.py
@@ -1,4 +1,5 @@
 from .data_preparation import DataPreparation
 from .nltk_data import NLTKResources
+from .model_ml import ModelML
 
-__all__ = ["DataPreparation", "NLTKResources"]
+__all__ = ["DataPreparation", "NLTKResources", "ModelML"]
