0f6401a0921c30b0e8fcd33a58350295ff333faa
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-13 12:49:58 +0300

    feat: Add preprocessing step to data preparation
    This commit adds a call to `data_preparation.preprocess_datasets()`
    after separating the data into training and testing sets. This ensures
    that both datasets are properly preprocessed before being used in
    subsequent steps. This step is crucial for improving model
    performance and ensuring data consistency.

--- main.py
+++ main.py
@@ -19,3 +19,5 @@ if __name__ == "__main__":
     data_preparation.add_binary_label(toxicity_list_column)
 
     data_preparation.separate_data_into_train_and_test()
+
+    data_preparation.preprocess_datasets()
