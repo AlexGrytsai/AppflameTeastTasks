3ed317564e70d5f0923dc73b77fd5102e84acda6
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-13 09:43:34 +0300

    feat: Add data download functionality for training
    Adds a method `download_data_for_training` to download the
    training data from a CSV file into a Pandas DataFrame. This
    facilitates data loading and preparation for model training.
    The function takes a path to the CSV file as an argument.

--- train_ml_model/data_preparation.py
+++ train_ml_model/data_preparation.py
@@ -2,6 +2,8 @@ import logging
 from typing import Optional, List
 
 import nltk
+import pandas as pd
+from pandas import DataFrame
 
 logger = logging.getLogger(__name__)
 
@@ -30,3 +32,18 @@ class DataPreparation:
         for resource in resources:
             logger.info(f"Downloading resource: {resource}")
             nltk.download(resource)
+
+    @staticmethod
+    def download_data_for_training(path: str = "train.csv") -> DataFrame:
+        """
+        Downloads a CSV file containing data for training the model, and
+        returns a Pandas DataFrame with the downloaded data.
+
+        Parameters:
+        path (str): The path to the CSV file to download. Defaults to
+                    "train.csv".
+
+        Returns:
+        DataFrame: A Pandas DataFrame containing the downloaded data.
+        """
+        return pd.read_csv(path)
