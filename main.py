62389a1261c3e5dd165ca539287567f391abe26c
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-13 11:50:53 +0300

    feat: Add binary label creation to DataPreparation
    Adds a binary label creation function to the `DataPreparation` class.
    This function takes a list of toxicity columns and adds a new column
    "is_toxic" that indicates whether a comment is toxic based on any of
    the specified toxicity labels. This change is necessary to simplify
    toxicity classification and allow for more flexible toxicity
    detection based on various labels.

--- main.py
+++ main.py
@@ -5,4 +5,15 @@ if __name__ == "__main__":
     nltk_data = NLTKResources()
     data_preparation = DataPreparation()
 
+    toxicity_list_column = [
+        "toxic",
+        "severe_toxic",
+        "obscene",
+        "threat",
+        "insult",
+        "identity_hate",
+    ]
+
     nltk_data.download_nltk_resources()
+
+    data_preparation.add_binary_label(toxicity_list_column)
