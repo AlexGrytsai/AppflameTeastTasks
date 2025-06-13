0f00a56223793d9702a465239184b251086be079
Alex Grytsai <a.grytsai1987@gmail.com>
2025-06-13 15:29:32 +0300

    feat: Implement and evaluate Logistic Regression model
    This commit introduces a Logistic Regression model for toxicity
    classification.
    
    The changes include:
    - Importing LogisticRegression from sklearn.linear_model.
    - Creating a ModelML instance with LogisticRegression,
      utilizing the prepared data.
    - Evaluating the model's performance using
      `model_lr.get_assessment_model_performance()`.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from sklearn.linear_model import LogisticRegression
+
 import settings  # noqa
-from train_ml_model import DataPreparation, NLTKResources
+from train_ml_model import DataPreparation, NLTKResources, ModelML
 
 if __name__ == "__main__":
     nltk_data = NLTKResources()
-    data_preparation = DataPreparation()
+    data_for_training = DataPreparation()
 
     toxicity_list_column = [
         "toxic",
@@ -16,8 +18,15 @@ if __name__ == "__main__":
 
     nltk_data.download_nltk_resources()
 
-    data_preparation.add_binary_label(toxicity_list_column)
+    data_for_training.add_binary_label(toxicity_list_column)
 
-    data_preparation.separate_data_into_train_and_test()
+    data_for_training.separate_data_into_train_and_test()
 
-    data_preparation.vectorize_data()
+    data_for_training.vectorize_data()
+
+    # Logistic Regression model
+    model_lr = ModelML(
+        LogisticRegression(C=1, max_iter=1000),
+        data_for_training,
+    )
+    model_lr.get_assessment_model_performance()
